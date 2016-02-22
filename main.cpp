8cf59f59680bc54c30522d5df1e6ea90c130557e
Mikhail <gordmisha@gmail.com>
2016-02-22 13:55:27 +0300

    22.02.2016 13:55:27

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ using std::vector;
 using std::thread;
 #include <mutex>
 using std::mutex;
+using std::unique_lock;
+#include <condition_variable>
+using std::condition_variable;
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::cerr;
 #include <queue>
 using std::queue;
 #include <string>
@@ -21,17 +25,41 @@ using std::ifstream;
 mutex M;
 
 template <typename T>
-class BlockingQueue {
+class myBlockingQueue {
 private:
   mutex mutex_;
   queue<T> queue_;
+  condition_variable cond_;
+  int readers_;
 public:
   T pop() {
+    /*unique_lock<mutex> mlock(mutex_);
+    while (queue_.empty()) {
+        cond_.wait(mlock);
+    }*/
     this->mutex_.lock();
     T value = this->queue_.front();
     this->queue_.pop();
     this->mutex_.unlock();
     return value;
+  }
+
+  myBlockingQueue(int r){
+    if (r < 0) {
+      cerr << "Need positive number";
+      exit(1);
+    }
+    readers_ = r;
+  }
+
+  bool any() {
+    return readers_ > 0;
+  }
+
+  void done() {
+    this->mutex_.lock();
+    this->readers_--;
+    this->mutex_.unlock();
   }
 
   void push(T value) {
@@ -48,8 +76,7 @@ public:
   }
 };
 
-void Reader(int i)
-{
+void Reader(int i, int n_vars, myBlockingQueue<string> &q) {
   M.lock();
   cout << "Hello Reader! Thread ID, " << i << endl;
   M.unlock();
@@ -60,34 +87,48 @@ void Reader(int i)
     M.lock();
     std::cout << "R" << i+1 << " " << s << std::endl;
     M.unlock();
+    q.push(s);
   }
+  q.done();
 }
 
-void Solver(int i)
-{
+void Solver(int i, int n_vars, myBlockingQueue<string> &q) {
+  sleep(1);
   M.lock();
   cout << "Hello Solver! Thread ID, " << i << endl;
   M.unlock();
+  string s;
+  while (!q.any()) {
+    s = q.pop();
+    M.lock();
+    std::cout << "S" << i+1 << " " << s << std::endl;
+    M.unlock();
+  }
 }
 
 int main ()
 {
-  int rc, ns = 4, nr = 3;
+  int ns = 4, nr = 3, n_vars = 3, i = 0;
   vector<thread> readers;
   vector<thread> solvers;
-  int i;
+  myBlockingQueue<string> q(nr);
+
   for( i=0; i < nr; i++ ) {
     M.lock();
     cout << "main() : creating reader, " << i << endl;
     M.unlock();
-    readers.push_back(thread(Reader, i));
+    readers.push_back(thread(Reader, i, n_vars, ref(q)));
   }
 
   for( i=0; i < ns; i++ ) {
     M.lock();
     cout << "main() : creating solver, " << i << endl;
     M.unlock();
-    solvers.push_back(thread(Solver, i));
+    solvers.push_back(thread(Solver, i, n_vars, ref(q)));
   }
-  sleep(1);
+  sleep(3);
+  while (!q.empty()) {
+    string s = q.pop();
+    cout<<s<<endl;
+  }
 }
